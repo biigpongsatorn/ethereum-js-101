5836494adc26dba41d084c60b9458f75dba9ac59
biig_pongsatorn <biig_pongsatorn@hotmail.com>
2018-07-05 20:33:41 +0700

    add whales list

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,22 @@
 <template>
   <div id="app">
     <b-loading :is-full-page="true" :active="isLoading > 0"></b-loading>
-    <div class="f-left w-100pct">
-      <Header/>
+    <div class="f-left w-20pct h-100vh pd-30px app--sidebar-container">
+      <div class="f-w-bold mg-bt-10px">Whales list</div>
+      <div class="app--whales-item" v-for="(addr, i) in whales" :key="i">
+        <WalletAddress :address="addr"/>
+      </div>
     </div>
-    <div class="f-left w-100pct mn-h-300calc">
-      <Content/>
-    </div>
-    <div class="f-left w-100pct">
-      <Footer/>
+    <div class="f-left w-80pct">
+      <div class="f-left w-100pct">
+        <Header/>
+      </div>
+      <div class="f-left w-100pct mn-h-300calc">
+        <Content/>
+      </div>
+      <div class="f-left w-100pct">
+        <Footer/>
+      </div>
     </div>
   </div>
 </template>
@@ -17,10 +25,24 @@
 import Header from '@/components/Header'
 import Content from '@/components/Content'
 import Footer from '@/components/Footer'
+import WalletAddress from '@/components/WalletAddress'
 import { mapGetters } from 'vuex'
 
 export default {
   name: 'App',
+  data () {
+    return {
+      whales: [
+        '0xb21FD5878D1e1B5Dbe1b702781AD1C2B53CEEa57',
+        '0xCA1767d9C960ff4Fb2C7BBA48DB354F7680102D0',
+        '0x736a376e4db6f1f52f746a75280774e91f9a2dfc',
+        '0x0d32cbf2b1d1458e61975f217afd03087e3340b2',
+        '0x5b63863f60dc27a2dd0678644a1a7753fe1e334a',
+        '0xe765825804182d16f2dfba8fc216f9abade567cb',
+        '0x1fe9c577b9b82a31447267f40664581269e92886'
+      ]
+    }
+  },
   computed: {
     ...mapGetters([
       'isLoading'
@@ -29,7 +51,20 @@ export default {
   components: {
     Header,
     Content,
-    Footer
+    Footer,
+    WalletAddress
   }
 }
 </script>
+
+<style scoped>
+.app--sidebar-container {
+  border-right: 1px solid #e6e6e6;
+}
+.app--whales-item {
+ padding: 5px;
+}
+.app--whales-item:hover {
+  color: #0facf3;
+}
+</style>
---
+++ src/components/WalletAddress.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="wallet-address--container">
+    <svg-filler path="/static/svg/wallet.svg" fill="#4a4a4a" width="18px" class="mg-r-5px"/>
+    {{ address }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'WalletAddress',
+  props: {
+    address: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style>
+.wallet-address--container {
+  width: 200px;
+  display: inline-block;
+  white-space: nowrap;
+  overflow: hidden !important;
+  text-overflow: ellipsis;
+  cursor: pointer;
+}
+</style>
