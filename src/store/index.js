250a8a497b668774e9c8c3c500ad3cdc7884fa10
biig_pongsatorn <biig_pongsatorn@hotmail.com>
2018-07-05 21:15:28 +0700

    add whales

--- src/store/index.js
+++ src/store/index.js
@@ -3,11 +3,13 @@ import * as actions from './actions'
 import * as mutations from './mutations'
 import * as getters from './getters'
 
+import whales from '../whales'
+
 const store = () => {
   return new Vuex.Store({
     state: {
       isLoading: 0,
-      addressSelected: '0xb21FD5878D1e1B5Dbe1b702781AD1C2B53CEEa57'
+      addressSelected: whales[0]
     },
     actions,
     mutations,
